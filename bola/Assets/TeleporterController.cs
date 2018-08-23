1a3e9c17e51d74998a4fb71265498f29766a5aea
Andres Miguel Manso <andres.miguel@neoris.net>
2018-08-23 09:02:31 +0200

    Roll a Ball and Survival Zombi demo and proyects uploaded

---
+++ bola/Assets/TeleporterController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterController : MonoBehaviour {
+
+    private bool teleportEnabled;
+    private bool firstClick;
+    private float firstClickTime;
+    private float doubleClickTimeLimit = 0.5f;
+    public VRTeleporter teleporter;
+    private void Start()
+    {
+        //teleportEnabled = false;
+        //firstClick = false;
+        //firstClickTime = 0f;
+        teleporter.ToggleDisplay(false);
+    }
+    public void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryThumbstick))
+        {
+            teleporter.ToggleDisplay(true);
+        }
+        if (OVRInput.Get(OVRInput.Button.SecondaryThumbstickUp))
+        {
+            teleporter.ToggleDisplay(true);
+        }
+        if (OVRInput.GetUp(OVRInput.Button.SecondaryThumbstick))
+        {
+            teleporter.Teleport();
+            teleporter.ToggleDisplay(false);
+        }
+        if (OVRInput.GetUp(OVRInput.Button.SecondaryThumbstickUp))
+        {
+            teleporter.Teleport();
+            teleporter.ToggleDisplay(false);
+        }
+        //UpdateTeleportEnabled();
+        //if(teleportEnabled)
+        //{
+        //    teleporter.ToggleDisplay(true);
+        //    if(OVRInput.Get(OVRInput.Button.SecondaryThumbstick, OVRInput.Controller.RTouch))
+        //    { teleporter.Teleport(); }
+
+        //}
+
+    }
+
+    //private void UpdateTeleportEnabled()
+    //{
+    //    if (OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.RTouch))
+    //    { // The trigger is pressed.
+    //        if (!firstClick)
+    //        { // The first click is detected.
+    //            firstClick = true;
+    //            firstClickTime = Time.unscaledTime;
+    //        }
+    //        else
+    //        { // The second click detected, so toggle teleport mode.
+    //            firstClick = false;
+    //            ToggleTeleportMode();
+    //        }
+    //    }
+
+    //    if (Time.unscaledTime - firstClickTime > doubleClickTimeLimit)
+    //    { // Time for the double click has run out.
+    //        firstClick = false;
+    //    }
+    //}
+
+    //private void ToggleTeleportMode()
+    //{
+    //    teleportEnabled = !teleportEnabled;
+        
+    //    if (!teleportEnabled)
+    //    {
+    //        teleporter.ToggleDisplay(false);
+    //    }
+    //}
+}
---
+++ bola/Library/Collab/Download/Assets/Roll_a_Ball/Scripts/Teleporter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+    public GameObject TeleportMarker;
+    public Transform Player;
+    public float RayLength = 50f;
+    private bool teleportEnabled;
+    //public AudioSource audioSource;
+    public AudioClip clip;
+
+    // Use this for initialization
+    void Start()
+    {
+        TeleportMarker.SetActive(false);
+        teleportEnabled = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {//push a button to activate(UpdateTeleportEnable)
+     //handleTeleport
+     //Teleport
+        if (OVRInput.GetDown(OVRInput.Button.One))
+        {
+            ToggleTeleportMode();
+        }
+        if (teleportEnabled)
+        { HandleTeleport(); }
+    }
+
+    
+
+    private void ToggleTeleportMode()
+    {
+        teleportEnabled = !teleportEnabled;
+        if(!teleportEnabled)
+        {
+            TeleportMarker.SetActive(false);
+        }
+    }
+
+    private void HandleTeleport()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, RayLength))
+        {
+            if (hit.collider.tag == "Ground")
+            {
+                if (!TeleportMarker.activeSelf)
+                {
+                    TeleportMarker.SetActive(true);
+                }
+                TeleportMarker.transform.position = hit.point;
+
+                if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger)) // Teleport to the position.
+                {
+                    //clip.;
+                    Vector3 markerPosition = TeleportMarker.transform.position;
+                    Player.position = new Vector3(markerPosition.x, Player.position.y,
+                        markerPosition.z);
+                }
+                else
+                {
+                    TeleportMarker.SetActive(false);
+                }
+            }
+            else
+            {
+                TeleportMarker.SetActive(false);
+            }
+        }
+    }
+
+   
+}
+
+
